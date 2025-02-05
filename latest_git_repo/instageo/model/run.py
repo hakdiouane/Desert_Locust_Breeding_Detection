8613fcf2307da10a1a173f80e75dbf8f02ede477
hakdiouane <hakdiouane@gmail.com>
2025-02-05 07:52:05 +0100

    fix run function script

--- latest_git_repo/instageo/model/run.py
+++ latest_git_repo/instageo/model/run.py
@@ -60,23 +60,63 @@ log.setLevel(logging.INFO)
 
 
 def check_required_flags(required_flags: List[str], config: DictConfig) -> None:
-    ...
-    # same as before
+    """Check if required flags are provided.
+
+    Args:
+        required_flags: A list of required command line arguments.
+
+    Raises:
+        An exception if at least one of the arguments is not set
+    """
+    for flag_name in required_flags:
+        if getattr(config, flag_name) == "None":
+            raise RuntimeError(f"Flag --{flag_name} is required.")
 
 
 def get_device() -> str:
-    ...
-    # same as before
+    """Selects available device."""
+    try:
+        import torch_xla.core.xla_model as xm  # noqa: F401
+
+        device = "tpu"
+        logging.info("TPU is available. Using TPU...")
+    except ImportError:
+        if torch.cuda.is_available():
+            device = "gpu"
+            logging.info("GPU is available. Using GPU...")
+        else:
+            device = "cpu"
+            logging.info("Neither GPU nor TPU is available. Using CPU...")
+    return device
 
 
 def eval_collate_fn(batch: tuple[torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor]:
-    ...
-    # same as before
+    """Evaluation DataLoader Collate Function.
+
+    Args:
+        batch (Tuple[Tensor]): A list of tuples containing features and labels.
+
+    Returns:
+        Tuple of (x,y) concatenated into separate tensors
+    """
+    data = torch.cat([a[0][0] for a in batch], 0)
+    labels = torch.cat([a[0][1] for a in batch], 0)
+    return data, labels
 
 
 def infer_collate_fn(batch: tuple[torch.Tensor]) -> tuple[torch.Tensor, torch.Tensor]:
-    ...
-    # same as before
+    """Inference DataLoader Collate Function.
+
+    Args:
+        batch (Tuple[Tensor]): A list of tuples containing features and labels.
+
+    Returns:
+        Tuple of (x,y) concatenated into separate tensors
+    """
+    data = torch.stack([a[0][0] for a in batch], 0)
+    labels = [a[0][1] for a in batch]
+    filepaths = [a[1] for a in batch]
+    return (data, labels), filepaths
 
 
 def create_dataloader(
@@ -84,12 +124,34 @@ def create_dataloader(
     batch_size: int,
     shuffle: bool = False,
     num_workers: int = 1,
-    collate_fn: Optional = None,
+    collate_fn: Optional[Callable] = None,
     pin_memory: bool = True,
 ) -> DataLoader:
-    ...
-    # same as before
+    """Create a DataLoader for the given dataset.
 
+    This function is a convenient wrapper around the PyTorch DataLoader class,
+    allowing for easy setup of various DataLoader parameters.
+
+    Args:
+        dataset (Dataset): The dataset to load data from.
+        batch_size (int): How many samples per batch to load.
+        shuffle (bool): Set to True to have the data reshuffled at every epoch.
+        num_workers (int): How many subprocesses to use for data loading.
+        collate_fn (Optional[Callable]): Merges a list of samples to form a mini-batch.
+        pin_memory (bool): If True, the data loader will copy tensors into CUDA pinned
+            memory.
+
+    Returns:
+        DataLoader: An instance of the PyTorch DataLoader.
+    """
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+        pin_memory=pin_memory,
+    )
 
 class CoAtNetSegmentationModule(pl.LightningModule):
     """
@@ -288,14 +350,74 @@ def main(cfg: DictConfig) -> None:
     checkpoint_path = cfg.checkpoint_path
 
     if cfg.mode == "stats":
-        ...
-        # same as before
+        train_dataset = InstaGeoDataset(
+            filename=train_filepath,
+            input_root=root_dir,
+            preprocess_func=partial(
+                process_and_augment,
+                mean=[0] * len(MEAN),
+                std=[1] * len(STD),
+                temporal_size=TEMPORAL_SIZE,
+                im_size=IM_SIZE,
+            ),
+            bands=BANDS,
+            replace_label=cfg.dataloader.replace_label,
+            reduce_to_zero=cfg.dataloader.reduce_to_zero,
+            no_data_value=cfg.dataloader.no_data_value,
+            constant_multiplier=cfg.dataloader.constant_multiplier,
+        )
+        train_loader = create_dataloader(
+            train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=1,
+        )
+        mean, std = compute_mean_std(train_loader)
+        print(mean)
+        print(std)
+        exit(0)
 
     if cfg.mode == "train":
-        ...
-        # Create train_dataset, valid_dataset as before
-        train_loader = create_dataloader(train_dataset, ...)
-        valid_loader = create_dataloader(valid_dataset, ...)
+        check_required_flags(["root_dir", "train_filepath", "valid_filepath"], cfg)
+        train_dataset = InstaGeoDataset(
+            filename=train_filepath,
+            input_root=root_dir,
+            preprocess_func=partial(
+                process_and_augment,
+                mean=MEAN,
+                std=STD,
+                temporal_size=TEMPORAL_SIZE,
+                im_size=IM_SIZE,
+            ),
+            bands=BANDS,
+            replace_label=cfg.dataloader.replace_label,
+            reduce_to_zero=cfg.dataloader.reduce_to_zero,
+            no_data_value=cfg.dataloader.no_data_value,
+            constant_multiplier=cfg.dataloader.constant_multiplier,
+        )
+
+        valid_dataset = InstaGeoDataset(
+            filename=valid_filepath,
+            input_root=root_dir,
+            preprocess_func=partial(
+                process_and_augment,
+                mean=MEAN,
+                std=STD,
+                temporal_size=TEMPORAL_SIZE,
+                im_size=IM_SIZE,
+            ),
+            bands=BANDS,
+            replace_label=cfg.dataloader.replace_label,
+            reduce_to_zero=cfg.dataloader.reduce_to_zero,
+            no_data_value=cfg.dataloader.no_data_value,
+            constant_multiplier=cfg.dataloader.constant_multiplier,
+        )
+        train_loader = create_dataloader(
+            train_dataset, batch_size=batch_size, shuffle=True, num_workers=1
+        )
+        valid_loader = create_dataloader(
+            valid_dataset, batch_size=batch_size, shuffle=False, num_workers=1
+        )
 
         # ---------------------------------------------------------------------
         # CHANGED: Use CoAtNetSegmentationModule instead of Prithvi
@@ -333,7 +455,29 @@ def main(cfg: DictConfig) -> None:
         trainer.fit(model, train_loader, valid_loader)
 
     elif cfg.mode == "eval":
-        ...
+        check_required_flags(["root_dir", "test_filepath", "checkpoint_path"], cfg)
+        test_dataset = InstaGeoDataset(
+            filename=test_filepath,
+            input_root=root_dir,
+            preprocess_func=partial(
+                process_test,
+                mean=MEAN,
+                std=STD,
+                temporal_size=TEMPORAL_SIZE,
+                img_size=cfg.test.img_size,
+                crop_size=cfg.test.crop_size,
+                stride=cfg.test.stride,
+            ),
+            bands=BANDS,
+            replace_label=cfg.dataloader.replace_label,
+            reduce_to_zero=cfg.dataloader.reduce_to_zero,
+            no_data_value=cfg.dataloader.no_data_value,
+            constant_multiplier=cfg.dataloader.constant_multiplier,
+            include_filenames=True,
+        )
+        test_loader = create_dataloader(
+            test_dataset, batch_size=batch_size, collate_fn=eval_collate_fn
+        )
         # CHANGED: Load CoAtNetSegmentationModule
         model = CoAtNetSegmentationModule.load_from_checkpoint(
             checkpoint_path,
@@ -352,9 +496,7 @@ def main(cfg: DictConfig) -> None:
         result = trainer.test(model, dataloaders=test_loader)
         log.info(f"Evaluation results:\n{result}")
 
-    elif cfg.mode in ["sliding_inference", "chip_inference"]:
-        # same logic, just change the class that is loaded
-        ...
+    elif cfg.mode in "sliding_inference":
         model = CoAtNetSegmentationModule.load_from_checkpoint(
             checkpoint_path,
             image_size=IM_SIZE,
@@ -369,7 +511,107 @@ def main(cfg: DictConfig) -> None:
             in_chans=len(BANDS) * cfg.dataloader.temporal_dim,
         )
         # Then do the inference logic as before...
-        ...
+        model.eval()
+        infer_filepath = os.path.join(root_dir, cfg.test_filepath)
+        assert (
+            os.path.splitext(infer_filepath)[-1] == ".json"
+        ), f"Test file path expects a json file but got {infer_filepath}"
+        output_dir = os.path.join(root_dir, "predictions")
+        os.makedirs(output_dir, exist_ok=True)
+        with open(os.path.join(infer_filepath)) as json_file:
+            hls_dataset = json.load(json_file)
+        for key, hls_tile_path in tqdm(
+            hls_dataset.items(), desc="Processing HLS Dataset"
+        ):
+            try:
+                hls_tile, _ = process_data(
+                    hls_tile_path,
+                    None,
+                    bands=cfg.dataloader.bands,
+                    no_data_value=cfg.dataloader.no_data_value,
+                    constant_multiplier=cfg.dataloader.constant_multiplier,
+                    mask_cloud=cfg.test.mask_cloud,
+                    replace_label=cfg.dataloader.replace_label,
+                    reduce_to_zero=cfg.dataloader.reduce_to_zero,
+                )
+            except rasterio.RasterioIOError:
+                continue
+            nan_mask = hls_tile == cfg.dataloader.no_data_value
+            nan_mask = np.any(nan_mask, axis=0).astype(int)
+            hls_tile, _ = process_and_augment(
+                hls_tile,
+                None,
+                mean=cfg.dataloader.mean,
+                std=cfg.dataloader.std,
+                temporal_size=cfg.dataloader.temporal_dim,
+                augment=False,
+            )
+            prediction = sliding_window_inference(
+                hls_tile,
+                model,
+                window_size=(cfg.test.img_size, cfg.test.img_size),
+                stride=cfg.test.stride,
+                batch_size=cfg.train.batch_size,
+                device=get_device(),
+            )
+            prediction = np.where(nan_mask == 1, np.nan, prediction)
+            prediction_filename = os.path.join(output_dir, f"{key}_prediction.tif")
+            with rasterio.open(hls_tile_path["tiles"]["B02_0"]) as src:
+                crs = src.crs
+                transform = src.transform
+            with rasterio.open(
+                prediction_filename,
+                "w",
+                driver="GTiff",
+                height=prediction.shape[0],
+                width=prediction.shape[1],
+                count=1,
+                dtype=str(prediction.dtype),
+                crs=crs,
+                transform=transform,
+            ) as dst:
+                dst.write(prediction, 1)
+    
+    elif cfg.mode == "chip_inference":
+        check_required_flags(["root_dir", "test_filepath", "checkpoint_path"], cfg)
+        output_dir = os.path.join(root_dir, "predictions")
+        os.makedirs(output_dir, exist_ok=True)
+        test_dataset = InstaGeoDataset(
+            filename=test_filepath,
+            input_root=root_dir,
+            preprocess_func=partial(
+                process_and_augment,
+                mean=MEAN,
+                std=STD,
+                temporal_size=TEMPORAL_SIZE,
+                im_size=cfg.test.img_size,
+                augment=False,
+            ),
+            bands=BANDS,
+            replace_label=cfg.dataloader.replace_label,
+            reduce_to_zero=cfg.dataloader.reduce_to_zero,
+            no_data_value=cfg.dataloader.no_data_value,
+            constant_multiplier=cfg.dataloader.constant_multiplier,
+            include_filenames=True,
+        )
+        test_loader = create_dataloader(
+            test_dataset, batch_size=batch_size, collate_fn=infer_collate_fn
+        )
+        model = CoAtNetSegmentationModule.load_from_checkpoint(
+            checkpoint_path,
+            image_size=IM_SIZE,
+            learning_rate=cfg.train.learning_rate,
+            freeze_backbone=cfg.model.freeze_backbone,
+            num_classes=cfg.model.num_classes,
+            temporal_step=cfg.dataloader.temporal_dim,
+            class_weights=cfg.train.class_weights,
+            ignore_index=cfg.train.ignore_index,
+            weight_decay=cfg.train.weight_decay,
+            coatnet_variant=cfg.model.coatnet_variant,
+            in_chans=len(BANDS) * cfg.dataloader.temporal_dim,
+        )
+        chip_inference(test_loader, output_dir, model, device=get_device())
+
 
 if __name__ == "__main__":
     main()
